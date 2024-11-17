afde97fd0267577c4b1a689d63d6e69cbaac1f88
EgorDubovik <posik.web.m@gmail.com>
2024-11-16 19:14:58 -0600

    refs

--- backup.sh
+++ backup.sh
@@ -4,18 +4,21 @@ set -a
 source .env
 set +a
 
-# Параметры подключения
+# Connection parameters
 MYSQL_USER="root"
 MYSQL_PASSWORD=$MYSQL_ROOT_PASSWORD
 MYSQL_HOST=mysql
 MYSQL_DATABASE=$MYSQL_DATABASE
 
-# Папка для хранения бекапов
+# Backup directory
 BACKUP_DIR="./backups"
 DATE=$(date +\%F_%H-%M-%S)
 
-# Создание директории для бекапов, если ее нет
+# Create the backup directory if it doesn't exist
 mkdir -p $BACKUP_DIR
 
-# Выполнение резервного копирования
+# Perform the backup
 docker exec mysql sh -c "exec mysqldump -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE" > "$BACKUP_DIR/db_backup_$DATE.sql"
+
+# Keep only the last 3 backup files and remove older ones
+ls -t $BACKUP_DIR/db_backup_*.sql | tail -n +4 | xargs -r rm -- 2>/dev/null
