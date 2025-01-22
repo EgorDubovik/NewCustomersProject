7c713c66ab69acf05f6484609a29ec7db80cd08f
Egor Dubovik <posik.web.m@gmail.com>
2025-01-22 15:28:35 -0600

    fixes on book appointment online frontend

--- frontend/src/pages/BookAppointment/CustomerInfo.tsx
+++ frontend/src/pages/BookAppointment/CustomerInfo.tsx
@@ -101,45 +101,46 @@ const CustomerInfo = () => {
 			<div className={loading ? 'blur-sm' : ''}>
 				<h2 className="font-bold">Enter your information</h2>
 				<div className="pt-4">
-					<div className="grid grid-cols-1 sm:grid-cols-2 gap-x-10 gap-y-6 p-3 px-6 bg-white rounded-lg shadow-lg">
-						<div className="col-span-2">
-							<div className="relative h-11 w-full ">
-								<input
-									name="name"
-									value={customerInfo.name}
-									onChange={onChange}
-									placeholder=""
-									className="peer h-full w-full border-b border-gray-400 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-blue-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100"
-								/>
-								<label className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-blue-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-									You name
-								</label>
-							</div>
+					<div className="grid grid-cols-1 gap-y-6 p-3 px-6 bg-white rounded-lg shadow-lg">
+						<div className="relative h-11 w-full ">
+							<input
+								name="name"
+								value={customerInfo.name}
+								onChange={onChange}
+								placeholder=""
+								className="peer h-full w-full border-b border-gray-400 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-blue-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100"
+							/>
+							<label className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-blue-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+								You name
+							</label>
 						</div>
-						<div className="">
-							<div className="relative h-11 w-full ">
-								<input
-									name="email"
-									onChange={onChange}
-									placeholder=""
-									className="peer h-full w-full border-b border-gray-400 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-blue-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100"
-								/>
-								<label className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-blue-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-									Email
-								</label>
+
+						<div className="grid grid-cols-1 sm:grid-cols-2 gap-x-2 gap-y-6">
+							<div className="">
+								<div className="relative h-11 w-full ">
+									<input
+										name="email"
+										onChange={onChange}
+										placeholder=""
+										className="peer h-full w-full border-b border-gray-400 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-blue-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100"
+									/>
+									<label className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-blue-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+										Email
+									</label>
+								</div>
 							</div>
-						</div>
-						<div className="">
-							<div className="relative h-11 w-full ">
-								<input
-									name="phone"
-									onChange={onChange}
-									placeholder=""
-									className="peer h-full w-full border-b border-gray-400 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-blue-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100"
-								/>
-								<label className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-blue-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-									Phone number
-								</label>
+							<div className="">
+								<div className="relative h-11 w-full ">
+									<input
+										name="phone"
+										onChange={onChange}
+										placeholder=""
+										className="peer h-full w-full border-b border-gray-400 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-blue-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100"
+									/>
+									<label className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-blue-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+										Phone number
+									</label>
+								</div>
 							</div>
 						</div>
 						<div className="">
@@ -168,7 +169,7 @@ const CustomerInfo = () => {
 								</label>
 							</div>
 						</div>
-						<div className="col-span-2 grid grid-cols-1 sm:grid-cols-3 gap-10">
+						<div className="grid grid-cols-3 gap-6">
 							<div className="relative h-11 w-full ">
 								<input
 									name="city"
@@ -202,17 +203,19 @@ const CustomerInfo = () => {
 									Zip code
 								</label>
 							</div>
-						</div>
-						<div className="col-span-2 pt-3 text-right">
-							<button
-								onClick={() => {
-									sendInfo();
-								}}
-								type="button"
-								className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 font-medium rounded-md text-sm px-3 py-1.5 me-2 mb-2"
-							>
-								Create appointment
-							</button>
+							<div className="col-span-3">
+								<div className="pt-3 text-right">
+									<button
+										onClick={() => {
+											sendInfo();
+										}}
+										type="button"
+										className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 font-medium rounded-md text-sm px-5 py-3 me-2 mb-2"
+									>
+										Create appointment
+									</button>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
--- frontend/src/pages/BookAppointment/Services.tsx
+++ frontend/src/pages/BookAppointment/Services.tsx
@@ -1,52 +1,73 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { CustomerContext } from './CustomerContext';
 import { CustomerContextType } from './@types';
 const Services = () => {
+	const { services, selectedServices, updateSelectedServices, setSliderIndex } = useContext(CustomerContext) as CustomerContextType;
+	const onselected = (id: number) => {
+		updateSelectedServices(id);
+	};
 
-   const {services, selectedServices, updateSelectedServices, setSliderIndex} = useContext(CustomerContext) as CustomerContextType;
-   const onselected = (id:number) => {
-      updateSelectedServices(id);
-   }
+	const nextPage = () => {
+		setSliderIndex(2);
+	};
 
-   const nextPage = () => {
-      setSliderIndex(2);
-   };
-   
-   return (
-      <div className="col-span-2 text-left p-2 pb-12" >
-         <h2 className='font-bold'>Select service</h2>
-         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-4'>
-            
-            {services.map((service,index) => {
-
-               return (
-               <div key={index} className='bg-white border rounded-lg shadow-md text-sm cursor-pointer' onClick={()=>{onselected(service.id)}} >
-                  <div className={"rounded-t-lg p-2 flex justify-between "+(selectedServices.includes(service.id) ? 'bg-blue-500 text-white' : 'text-gray-800')}>
-                     <div className='font-bold text-lg pl-4'>
-                        {service.title}
-                     </div>
-                     <div className={'selected '+(selectedServices.includes(service.id) ? 'visible' : 'invisible')}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                           <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                        </svg>
-                     </div>
-                  </div>
-                  <div className='text-gray-700 pl-6 p-2 '>{service.description}</div>
-                  <div className='text-blue-600 pl-6 pb-2  text-lg'>${service.price}</div>
-               </div>);
-            })}
-
-         </div>
-         <div className='text-right fixed bottom-0 left-0 w-full pl-4 pr-4'>
-            { selectedServices.length > 0 
-               ? 
-               <button onClick={nextPage} type="button" className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 font-medium rounded-md text-sm px-3 py-1.5 me-2 mb-2 w-full">continue</button>
-               :
-               <button type="button" className="text-white bg-blue-300 font-medium rounded-md text-sm px-3 py-1.5 me-2 mb-2 w-full" disabled>Continue</button>
-            }
-         </div>
-      </div>
-   );
+	return (
+		<div className="col-span-2 text-left p-2 pb-12">
+			<h2 className="font-bold">Select service</h2>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-4">
+				{services.map((service, index) => {
+					return (
+						<div
+							key={index}
+							className="bg-white border rounded-lg shadow-md text-sm cursor-pointer"
+							onClick={() => {
+								onselected(service.id);
+							}}
+						>
+							<div className={'rounded-t-lg p-2 flex justify-between ' + (selectedServices.includes(service.id) ? 'bg-blue-500 text-white' : 'text-gray-800')}>
+								<div className="font-bold text-lg pl-4">{service.title}</div>
+								<div className={'selected ' + (selectedServices.includes(service.id) ? 'visible' : 'invisible')}>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+										<path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+									</svg>
+								</div>
+							</div>
+							<div className="text-gray-700 pl-6 p-2 ">{service.description}</div>
+							<div className="text-blue-600 pl-6 pb-2  text-lg">${service.price}</div>
+						</div>
+					);
+				})}
+			</div>
+			<div className="justify-end mt-10 hidden md:flex">
+				{selectedServices.length > 0 ? (
+					<button onClick={nextPage} type="button" className="flex items-center text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 font-medium rounded-md text-sm px-10 py-2 me-2 mb-2">
+						Continue{' '}
+						<svg className="ml-2" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed">
+							<path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+						</svg>
+					</button>
+				) : (
+					<button type="button" className="flex items-center text-white bg-blue-300 font-medium rounded-md text-sm px-10 py-2 me-2 mb-2" disabled>
+						Continue{' '}
+						<svg className="ml-2" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e8eaed">
+							<path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+						</svg>
+					</button>
+				)}
+			</div>
+			<div className="text-right fixed md:hidden bottom-0 left-0 w-full pl-4 pr-4">
+				{selectedServices.length > 0 ? (
+					<button onClick={nextPage} type="button" className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 font-medium rounded-md text-sm px-3 py-1.5 me-2 mb-2 w-full">
+						Continue
+					</button>
+				) : (
+					<button type="button" className="text-white bg-blue-300 font-medium rounded-md text-sm px-3 py-1.5 me-2 mb-2 w-full" disabled>
+						Continue
+					</button>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Services;
