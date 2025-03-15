ef365e7f914e165a0c85aea93b9ea6cad5eea24b
Egor Dubovik <posik.web.m@gmail.com>
2025-03-15 09:07:39 -0500

    add daily_payments_sum_table

---
+++ backend/database/migrations/2025_03_15_083738_create_dayly_payments_sum_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('daily_payments_sum', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->integer('company_id');
+            $table->decimal('total', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('daily_payments_sum');
+    }
+};
