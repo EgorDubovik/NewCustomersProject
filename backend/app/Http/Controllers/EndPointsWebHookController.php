f802967c1c56554b435a321f2f41d79db808ec08
Egor Dubovik <posik.web.m@gmail.com>
2025-05-24 16:40:38 -0500

    change endpoint return

--- backend/app/Http/Controllers/EndPointsWebHookController.php
+++ backend/app/Http/Controllers/EndPointsWebHookController.php
@@ -43,8 +43,10 @@ class EndPointsWebHookController extends Controller
 		}
 
 		return response()->json([
-			'message' => 'Appointments that already booked',
-			'appointments' => $returnData,
+			'result' => [
+				'message' => 'Appointments that already booked',
+				'appointments' => $returnData,
+			]
 		]);
 	}
 }
