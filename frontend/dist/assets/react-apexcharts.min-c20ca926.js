2ea917a9debd113ae1ece5577ac265ee64c4163f
Egor Dubovik <posik.web.m@gmail.com>
2025-08-07 14:44:00 -0500

    test deploy

--- frontend/dist/assets/react-apexcharts.min-c20ca926.js
+++ frontend/dist/assets/react-apexcharts.min-c20ca926.js
@@ -1,4 +1,4 @@
-import{r as qt,a7 as Zt}from"./index-67646ff4.js";var ht={},Ne={exports:{}};/*!
+import{r as qt,a7 as Zt}from"./index-aa3ff075.js";var ht={},Ne={exports:{}};/*!
  * ApexCharts v3.44.0
  * (c) 2018-2023 ApexCharts
  * Released under the MIT License.
